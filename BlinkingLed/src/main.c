31c0a548315e29a1e903c416be820e38f401e645
JPREmbDev <121400656+JPREmbDev@users.noreply.github.com>
2023-12-04 19:08:52 +0100

    Blinking Led

---
+++ BlinkingLed/src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN GPIO_NUM_2
+
+void app_main()
+{
+    // La siguiente linea de codigo config. el GPIO2 (led0) como salida
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while(1)
+    {
+        //led0 1 ENCENDIDO, 0 APAGADO
+        gpio_set_level(LED_PIN, 1);
+
+        //DELAY -> en milisegundos -> 0,5 s
+        vTaskDelay(500/portTICK_PERIOD_MS);
+
+        //led0 1 ENCENDIDO, 0 APAGADO
+        gpio_set_level(LED_PIN, 0);
+
+        //DELAY -> en milisegundos -> 0,5 s
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+
+}
